429d33fdd358b9bbc4e596e27d818dd4fc365c9d
Mayank Mittal <mmittal123456@gmail.com>
2022-05-13 12:04:18 +0530

    update frontend

--- app/src/screens/Home.tsx
+++ app/src/screens/Home.tsx
@@ -1,23 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Heading, HStack, Image, VStack, Box } from '@chakra-ui/react';
 import { useAnchorWallet, useWallet, useConnection } from '@solana/wallet-adapter-react';
+import * as anchor from "@project-serum/anchor";
+import { Connection, PublicKey } from '@solana/web3.js';
 import { Link } from 'react-router-dom';
-import { Lazycon } from '../utils/lazycon';
-import lazycon from '../utils/lazycon.json'
+import { Lazycon } from '../../../target/types/lazycon';
+import { idl } from '../../idl'
 import {
-  Program,
-  Provider,
-  BN,
-  web3,
-} from '@project-serum/anchor'
+  Program, AnchorProvider, web3, Wallet, Idl
+} from '@project-serum/anchor';
 import 'dotenv/config'
-const PROGRAM_ID = process.env.PROGRAM_ID
+const PROGRAM_ID = new PublicKey(idl.metadata.address)
+const { SystemProgram, Keypair } = web3;
+
 
 export const Home = () => {
-  const wallet = useWallet();
-  const connection = useConnection();
-  const provider = 
-  const program = new Program(lazycon,PROGRAM_ID,)
+  const { publicKey, wallet, signTransaction, signAllTransactions } =
+    useWallet();
+  const [anchorProgram, setAnchorProgram] = useState<any>(null);
+  const [provider, setProvider] = useState<any>();
+  const { connection } = useConnection();
+  const signerWallet = {
+    publicKey: publicKey,
+    signTransaction: signTransaction,
+    signAllTransactions: signAllTransactions,
+  };
+  const getProvider = () => {
+    console.log("Getting provider");
+    if (!wallet || !publicKey || !signTransaction || !signAllTransactions) {
+      return;
+    }
+
+    const signerWallet = {
+      publicKey: publicKey,
+      signTransaction: signTransaction,
+      signAllTransactions: signAllTransactions,
+    };
+
+    const provider = new anchor.AnchorProvider(connection, signerWallet, {
+      commitment: "processed",
+    });
+
+    console.log("provider",provider);
+
+    setProvider(provider);
+  };
+
+  const loadAnchor = async () => {
+    const programId = new PublicKey(idl.metadata.address);
+
+    if (provider) {
+      const myProgram = new anchor.Program(idl, programId, provider);
+
+      console.log(myProgram);
+      setAnchorProgram(myProgram);
+    }
+  };
+
+  useEffect(() => {
+    getProvider();
+  }, [wallet, publicKey]);
+
+  useEffect(() => {
+    loadAnchor();
+  }, [provider]);
+
+  useEffect(() => {
+    console.log(anchorProgram);
+  }, [anchorProgram]);
+
+  useEffect(() => {
+    console.log(provider);
+  }, [provider]);
+
   return (
     <Box p={10}>
 
@@ -29,8 +84,7 @@ export const Home = () => {
           spacing={0}
         >
           <Heading fontSize={50} fontWeight="extrabold" pb={50}>
-            Protect your Intellectual Property Rights using Blockchain. Create
-            Patents as NFTs on Solana.
+            Your Account
           </Heading>
           <HStack>
             <Button size="lg" variant="outline" as={Link} to="/explore">
